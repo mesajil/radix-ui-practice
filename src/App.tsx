486cf9f706583b0cdb48dafcd5d90e9c67a9d820
Luis Mesajil <lhmesajil@gmail.com>
2024-10-02 21:09:26 -0500

    feat(Button): Add asChild and ref props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Button } from './components/ui/Button'
 import './App.css'
+import { Textarea } from './components/ui/textarea'
 
 function App() {
   const hello = () => alert('Hello World!')
   return (
     <>
+      <Textarea />
       <Button onClick={hello}>My Default Button</Button>
       <Button onClick={hello} size={'large'} variant={'secondary'}>
         My Big Button
       </Button>
-      <Button onClick={hello} size={'small'} variant={'outline'} className='text-red-400'>
+      <Button size={'small'} variant={'outline'} className='text-red-400'>
         My Outline Button
+      </Button>
+      <Button asChild size={'large'} variant={'outline'}>
+        <a href='https://github.com/mesajil' target='_blank' rel='noopener noreferrer'>
+          GitHub
+        </a>
       </Button>
     </>
   )
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,9 @@
-import { ComponentProps, FC } from 'react'
+import React, { ComponentProps, FC } from 'react'
 import { cva, VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
+import { Slot } from '@radix-ui/react-slot'
 
-const buttonStyles = cva('px-4 py-2 font-semibold rounded', {
+const buttonVariants = cva('px-4 py-2 font-semibold rounded', {
   variants: {
     size: {
       small: 'text-xs',
@@ -21,8 +22,16 @@ const buttonStyles = cva('px-4 py-2 font-semibold rounded', {
   },
 })
 
-interface ButtonProps extends ComponentProps<'button'>, VariantProps<typeof buttonStyles> {}
-
-export const Button: FC<ButtonProps> = ({ className, size, variant, ...props }) => {
-  return <button className={cn(buttonStyles({ size, variant, className }))} {...props} />
+interface ButtonProps extends ComponentProps<'button'>, VariantProps<typeof buttonVariants> {
+  asChild?: boolean
 }
+
+const Button: FC<ButtonProps> = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, size, variant, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button'
+    return <Comp className={cn(buttonVariants({ size, variant, className }))} ref={ref} {...props} />
+  },
+)
+
+Button.displayName = 'Button'
+export { Button, buttonVariants }
